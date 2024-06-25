23914a4ea739f6e5b641292226e50879a4d43340
farnooshoa <97453982+farnooshoa@users.noreply.github.com>
2024-06-24 22:31:43 -0400

    Update complement.py

--- Rosalind/Stronghold/complement.py
+++ Rosalind/Stronghold/complement.py
@@ -1,3 +1,12 @@
+'''
+A solution to a ROSALIND bioinformatics problem.
+
+Problem Title: Complementing a Strand of DNA
+Rosalind ID: REVC
+Rosalind #: 003
+URL: http://rosalind.info/problems/revc/
+'''
+
 def complement (text):
    return text.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c').upper()[::-1]
 
