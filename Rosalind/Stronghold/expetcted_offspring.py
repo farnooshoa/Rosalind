8ff477afdaa6e6acdc8c8005befc195db873d3a8
farnooshoa <97453982+farnooshoa@users.noreply.github.com>
2024-06-24 22:18:04 -0400

    Add files via upload

---
+++ Rosalind/Stronghold/expetcted_offspring.py
@@ -0,0 +1,20 @@
+def calculate_expected_offspring(AA_AA, AA_Aa, AA_aa, Aa_Aa, Aa_aa, aa_aa):
+    # Calculate the expected number of dominant offspring for each genotype
+    expected_AA_AA = AA_AA * 2 * 1  # 2 offspring, both dominant
+    expected_AA_Aa = AA_Aa * 2 * 1  # 2 offspring, both dominant
+    expected_AA_aa = AA_aa * 2 * 1  # 2 offspring, both dominant
+    expected_Aa_Aa = Aa_Aa * 2 * 0.75  # 2 offspring, 75% dominant
+    expected_Aa_aa = Aa_aa * 2 * 0.5  # 2 offspring, 50% dominant
+    expected_aa_aa = aa_aa * 2 * 0  # 2 offspring, 0% dominant
+
+    # Sum up the expected number of dominant offspring for all genotypes
+    total_expected_offspring = (
+        expected_AA_AA + expected_AA_Aa + expected_AA_aa +
+        expected_Aa_Aa + expected_Aa_aa + expected_aa_aa
+    )
+
+    return total_expected_offspring
+
+# Example usage with given numbers
+result = calculate_expected_offspring(19710,16445,17978,18181,16326,17644)
+print(result)
---
+++ Rosalind/Stronghold/rabbit.py
@@ -0,0 +1,16 @@
+def rabbit_pairs(n, k):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        fib_sequence = [1, 1]
+        for i in range(2, n):
+            fib_sequence.append(fib_sequence[-1] + k * fib_sequence[-2])
+
+        return fib_sequence[-1]
+
+# Example usage:
+n_value = 4
+
+k_value = 2
+result = rabbit_pairs(n_value, k_value)
+print(result)
